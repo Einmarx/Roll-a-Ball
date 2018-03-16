d1506ee0cc2efb4c023859a410983913dd453edd
Oliver Enemærke <Einmarx@users.noreply.github.com>
2018-03-16 13:09:51 +0100

    Master

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown ("space"))
+        {
+            Vector3 jump = new Vector3(0.0f, 300.0f, 0.0f);
+            GetComponent<Rigidbody>().AddForce(jump);
+        }
+     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}

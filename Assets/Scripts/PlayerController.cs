ddaf2109742bc68c4b1cf1b258abbfa4b452858c
Oliver Lützhøft Enemærke <Einmarx@users.noreply.github.com>
2018-03-16 17:07:04 +0100

    Final Game?
    Might be the final update to the game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -10,10 +11,23 @@ public class PlayerController : MonoBehaviour {
 
     bool isGrounded;
 
+    public AudioSource pickupsound;
+
+    private int Count;
+
+    public Text countText;
+
+    public Text WinText;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+        pickupsound = GetComponent<AudioSource>();
+        Count = 0;
+        SetCountText();
+        WinText.text = "";
+     }
 
     void FixedUpdate()
     {
@@ -40,7 +54,7 @@ public class PlayerController : MonoBehaviour {
     {
         if (collision.gameObject.CompareTag("Floor")) ;
         {
-            isGrounded = true;
+           isGrounded = true;
         }
     }
 
@@ -48,7 +62,7 @@ public class PlayerController : MonoBehaviour {
     {
         if (collision.gameObject.CompareTag("Floor")) ;
         {
-            isGrounded = false;
+           isGrounded = false;
         }
     }
 
@@ -56,7 +70,19 @@ public class PlayerController : MonoBehaviour {
     {
         if (other.gameObject.CompareTag ("Pick Up"))
         {
+            pickupsound.Play();
             other.gameObject.SetActive(false);
+            Count = Count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count:" + Count.ToString();
+        if (Count >= 12)
+        {
+            WinText.text = "You Win!";
         }
     }
 }
